e0b846582a38d804bbc76fa2c5479be4e10ed321
sofia <sobala0987654321@gmail.com>
2025-04-07 22:04:31 +0700

    fix: errors in sorter

--- src/utils.ts
+++ src/utils.ts
@@ -38,16 +38,18 @@ export const getNearOffers = (offers: OfferCommonInfo[], index: number) => ([...
 
 export const getCurrentCityOffers = (offers: OfferCommonInfo[], currentCity: CityNameType) => offers.filter((offer: OfferCommonInfo) => offer.city.name === currentCity);
 
+export const sortOfferByDefault = (offers: OfferCommonInfo[]) => (offers);
+
 export const sortOfferByHigh = (offers: OfferCommonInfo[]) => (offers.sort((a,b) => b.price - a.price));
 
-export const sortOfferByLow = (offers: OfferCommonInfo[]) => (offers.sort((a,b) => b.price - a.price));
+export const sortOfferByLow = (offers: OfferCommonInfo[]) => (offers.sort((a,b) => a.price - b.price));
 
 export const sortOfferByRating = (offers: OfferCommonInfo[]) => (offers.sort((a,b) => b.rating - a.rating));
 
 export const getSorter = (sortByType: FilterOptionsDescriptionType) => {
   switch(sortByType){
     case FILTER_OPTIONS.popular:
-      return (offers: OfferCommonInfo[]) => offers;
+      return sortOfferByDefault;
     case FILTER_OPTIONS.high:
       return sortOfferByHigh;
     case FILTER_OPTIONS.low:
@@ -55,6 +57,6 @@ export const getSorter = (sortByType: FilterOptionsDescriptionType) => {
     case FILTER_OPTIONS.top:
       return sortOfferByRating;
     default:
-      return (offers: OfferCommonInfo[]) => offers;
+      return sortOfferByDefault;
   }
 };
